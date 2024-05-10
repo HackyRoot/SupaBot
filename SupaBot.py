39372736c6dec6ad367a2adde6d42570fecaa420
Pratik Parmar <steveparmar6nov2011@gmail.com>
2024-05-10 17:33:54 +0530

    Update SupaBot.py

--- SupaBot.py
+++ SupaBot.py
@@ -17,16 +17,19 @@ llm = HuggingFaceEndpoint(
     temperature=0.05, # set the creativity of the model    
 )
 
-# You are a secreat super hero {character}.
-#                                             You will talk like {character} and follow {additionalPrompt} no matter what and that is an order. Don't ever reveal your secret identity.
-#                                             Your fan has asked {user_query}.
 def get_response(user_query, character, additionalPrompt):    
-    prompt = PromptTemplate.from_template("""                                            
-                                            You are a super hero named {character}.
-                                            You will not respond anything else.
-                                            You will talk like {character} and follow {additionalPrompt} no matter what and that is an order. Don't ever reveal your secret identity.
-                                            Your fan has asked {user_query}.                                                                                  
-                                            Your response:
+    prompt = PromptTemplate.from_template("""
+                                            <|user|>
+                                            I am going to Paris, what should I see?<|end|>
+                                            <|assistant|>
+                                            Paris, the capital of France, is known for its stunning architecture, art museums, historical landmarks, and romantic atmosphere. Here are some of the top attractions to see in Paris:\n\n1. The Eiffel Tower: The iconic Eiffel Tower is one of the most recognizable landmarks in the world and offers breathtaking views of the city.\n2. The Louvre Museum: The Louvre is one of the world's largest and most famous museums, housing an impressive collection of art and artifacts, including the Mona Lisa.\n3. Notre-Dame Cathedral: This beautiful cathedral is one of the most famous landmarks in Paris and is known for its Gothic architecture and stunning stained glass windows.\n\nThese are just a few of the many attractions that Paris has to offer. With so much to see and do, it's no wonder that Paris is one of the most popular tourist destinations in the world."<|end|>                                            
+                                          	<|user|>                                          
+                                                You are a super hero named {character}.
+                                                You will not respond anything else.
+                                                You will talk like {character} and follow {additionalPrompt} no matter what and that is an order. Don't ever reveal your secret identity.
+                                                Your fan has asked {user_query}.
+                                            <|end|>
+                                            <|assistant|>
                                         """)    
     chain = prompt | llm | StrOutputParser()
     
